e8b632cee1084a89f930ca801d6a208216af3868
SmitModi-7 <smitmodi.ict18@gmail.com>
2024-09-09 00:59:26 +0530

    Linting errors

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -8,31 +8,36 @@ from django.db.utils import OperationalError
 
 from psycopg2 import OperationalError as Psycopg2OpError
 
+
 class Command(BaseCommand):
     """Django command to wait for database"""
 
     def handle(self, *args, **options):
-        """Entrypoint for command"""    
+        """Entrypoint for command"""
         self.stdout.write('Waiting for database...')
         db_up = False
         max_retries = 30
         retry = 0
 
-        #Keep Trying to connect to database until max no of tries
+        # Keep Trying to connect to database until max no of tries
         while db_up is False and retry < max_retries:
             try:
-                #Checks whether our database is ready, if it is not ready then it will throw the exceptions
+                ''' Checks whether our database is ready, if it is not ready then
+                    it will throw the exceptions '''
                 self.check(databases=['default'])
                 db_up = True
-            except(Psycopg2OpError,OperationalError) as ex:
+            except(Psycopg2OpError, OperationalError) as ex:
                 self.stdout.write(
                     "Database unavailable on attempt {attempt}/{max_retries}:"
-                    " {error}".format(attempt=retry + 1, max_retries=max_retries, error=ex)
+                    "{error}".format(
+                      attempt=retry + 1,
+                      max_retries=max_retries, error=ex
+                    )
                 )
                 time.sleep(1)
                 retry += 1
 
-        #Log to the terminal/Command line wheter Database is available or not
+        # Log to the terminal/Command line wheter Database is available or not
         if db_up:
             self.stdout.write(self.style.SUCCESS('Database is available...'))
         else:
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,37 +3,40 @@ Test custom Django management commands
 """
 
 from django.db.utils import OperationalError
-#Helper function to call django commands
+
 from django.core.management import call_command
 from django.test import SimpleTestCase
 
 from psycopg2 import OperationalError as pyscopg2OpError
 from unittest.mock import patch
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test Commands."""
 
-    def test_wait_for_db_ready(self,patched_check):
+    def test_wait_for_db_ready(self, patched_check):
         """Tests whether DB is ready for connection"""
 
-        #Calling wait for db command
+        # Helper function to call django commands
+        # Calling wait for db command
         call_command('wait_for_db')
-        #Checking if check method is called once with correct database
+        # Checking if check method is called once with correct database
         patched_check.assert_called_once_with(databases=['default'])
 
-    #Mocking sleep function so the tests don't have to wait
+    # Mocking sleep function so the tests don't have to wait
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Tests delay in DB connection due to any operational error"""
-        
-        #Generating error 5 times, then returning true
+
+        # Generating error 5 times, then returning true
         patched_check.side_effect = 2 * [pyscopg2OpError] + \
             3 * [OperationalError] + [True]
-        
+
         call_command('wait_for_db')
-        
-        #Database should be checked exactly 6 times, First 5 times it gave error and other time database was ready.
+
+        ''' Database should be checked exactly 6 times, First 5 times it
+            gave error and other time database was ready. '''
         self.assertEqual(patched_check.call_count, 6)
-        #Checking if check method is called with correct database
+        # Checking if check method is called with correct database
         patched_check.assert_called_with(databases=['default'])
