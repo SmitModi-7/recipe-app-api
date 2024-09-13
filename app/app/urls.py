235e64f4792138cc923fc951e9aa6fc9544e8f81
SmitModi-7 <smitmodi.ict18@gmail.com>
2024-09-13 08:51:48 +0530

    Added/Configured Documentation for APIs ( DRF Spectacular and Swagger UI )

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView
+)
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Generating schema file
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    # Serving swagger documentation with the generated schema file
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs'
+        ),
 ]
